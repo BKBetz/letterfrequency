3c761cfafca58f8e9044cdca81cd31c4016e6e60
AHipWhale <charlie.choffat@gmail.com>
2021-05-12 15:08:53 +0200

    completed reducer.py

--- reducer.py
+++ reducer.py
@@ -1,24 +1,10 @@
 import numpy as np
 
 def reduce(new_matrix):
+    """Adds matrices together and gives the average"""
     global matrix
     try:
-        print(matrix, new_matrix)
-        result = [[(matrix[i][j] + new_matrix[i][j]) / 2 for j in range(len(matrix[0]))] for i in range(len(matrix))]
+        result = np.array([[(matrix[i][j] + new_matrix[i][j]) / 2 for j in range(len(matrix[0]))] for i in range(len(matrix))]).reshape((28, 28))
         return result
     except NameError:
         return new_matrix
-
-
-m = np.matrix([[1, 2], [3, 4]])
-print(m)
-# matrix = reduce(m)
-
-
-
-
-
-# print('na 1 keer',matrix)
-# ma = np.matrix([[5, 4], [3, 2]])
-# matrix = reduce(ma)
-# print('na 2 keer', matrix)
