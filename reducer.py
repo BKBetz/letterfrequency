411774ac62fc93011084e80d39548a0610b66cea
AHipWhale <charlie.choffat@gmail.com>
2021-05-12 16:07:13 +0200

    fixed reducer.py

--- reducer.py
+++ reducer.py
@@ -1,10 +1,16 @@
 import numpy as np
 
+matrix = None
+first_matrix = True
+
 def reduce(new_matrix):
     """Adds matrices together and gives the average"""
-    global matrix
-    try:
-        result = np.array([[(matrix[i][j] + new_matrix[i][j]) / 2 for j in range(len(matrix[0]))] for i in range(len(matrix))]).reshape((28, 28))
-        return result
-    except NameError:
-        return new_matrix
+    global matrix, first_matrix
+
+    if first_matrix == True:
+        matrix = new_matrix
+        first_matrix = False
+
+    else:
+        matrix = np.array([[(matrix[i][j] + new_matrix[i][j]) / 2 for j in range(len(matrix[0]))] for i in
+                           range(len(matrix))]).reshape((2, 2))
