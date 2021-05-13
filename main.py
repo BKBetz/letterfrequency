70ce9864a429c598461f2067d0bb3e6808337db1
BKBetz <brandonbetz@live.nl>
2021-05-13 13:44:23 +0200

    made change to test function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from letterfrequency.mapper import mapper
 from letterfrequency.reducer import onebyonereduce
+from letterfrequency.matrix_test import answer
 
 english_matrix = onebyonereduce(mapper('../data/alice.txt'))
-print(english_matrix)
+dutch_matrix = onebyonereduce(mapper())
+test = "my banana is krom omdat brandon is a lazy varken"
+print(answer(test, english_matrix))
