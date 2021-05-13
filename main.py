a613ebee38285107326c8235b90217bdfa8dcf53
BKBetz <brandonbetz@live.nl>
2021-05-13 13:54:26 +0200

    added dutch matrix

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from letterfrequency.reducer import onebyonereduce
 from letterfrequency.matrix_test import answer
 
 english_matrix = onebyonereduce(mapper('../data/alice.txt'))
-dutch_matrix = onebyonereduce(mapper())
+dutch_matrix = onebyonereduce(mapper('../data/bzt.txt'))
 test = "my banana is krom omdat brandon is a lazy varken"
-print(answer(test, english_matrix))
+print(answer(test, english_matrix, dutch_matrix))
